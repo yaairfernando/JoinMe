914eec819b059cb1a76fa27d568d7799584b4fa8
Yair Facio <yair.facio11@gmail.com>
2020-03-10 19:19:48 -0600

    Adding bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,7 +4,8 @@ const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
-    jQuery: 'jquery'
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
   })
 )
 

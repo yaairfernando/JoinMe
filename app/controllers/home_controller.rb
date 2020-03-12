6ecd6629ebc93240695044838a4b27956053f0ba
Yair Facio <yair.facio11@gmail.com>
2020-03-11 19:51:14 -0600

    Adding carousel with the recent events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @events = Event.all.order('created_at DESC')
+    @events = Event.all.order('created_at DESC').limit(5)
     @event = Event.first
   end
 end

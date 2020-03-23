74ff7993c282df360203ac3d326a8504c001d1f5
Yair Facio <yair.facio11@gmail.com>
2020-03-23 13:33:06 -0600

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  resources :events
+  root 'home#index'
   get 'sign_up', to: 'users#new'
   post 'sign_up', to: 'users#create'
-
   get 'user_profile/:id', to: 'sessions#show'
+  
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
 
-  root 'home#index'
+  get 'events/:id/invite', to: 'events#invite', as: 'invite_event'
+  get 'events/:id/attend', to: 'events#attend', as: 'attend_event'
+  get 'users/invitations', to: 'users#invited_events', as: 'invited_events'
+  
+  resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

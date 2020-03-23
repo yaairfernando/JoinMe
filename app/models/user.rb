b5b1ca3afa3927340d72cb2a5f666be9fbf16593
Yair Facio <yair.facio11@gmail.com>
2020-03-23 13:29:22 -0600

    Added helper methods to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,20 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false }
 
   has_many :events, foreign_key: :creator_id, dependent: :destroy
-  has_many :invitations, foreign_key: :attendee_id
+  has_many :invitations, foreign_key: :attendee_id, dependent: :destroy
   has_many :invited_events, through: :invitations, source: :event
   has_many :attended_events, through: :invitations, source: :event
-
-  
 
   private
   def down_case
     email.downcase!
+  end
+
+  def upcoming_events
+    attended_events.where('date >= ? AND accepted = ?', DateTime.now, true)
+  end
+
+  def previous_events
+    attended_events.where('date < ? AND accepted = ?', DateTime.now, true)
   end
 end

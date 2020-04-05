ed032c960d475fbdc29019b46bdafcf11f25a2f1
Yair Facio <yair.facio11@gmail.com>
2020-04-04 20:10:39 -0600

    Added comment create and destroy actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,21 @@
 class CommentsController < ApplicationController
   def create
+    @event = Event.find(params[:event_id])
+    @comment = @event.comments.create(comment_params)
+    @comment.creator_id = current_user.id if current_user
+    @comment.save!
+    flash[:success] = 'New comment added'
   end
 
   def destroy
+    @event = Event.find(params[:event_id])
+    @comment = @event.comments.find(params[:id])
+    @comment.destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
